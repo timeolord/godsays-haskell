ea02376fbc95231a59f1d5c30b557d04f4419a8f
timeolord <timeolord6677@gmail.com>
2025-11-10 12:57:49 -0500

    changed to flakes

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Haskell Project Shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          packages = [
+            dos2unix
+            (haskellPackages.ghcWithPackages (
+              pkgs: with pkgs; [
+                cabal-install
+              ]
+            ))
+          ];
+        };
+      }
+    );
+}
